0c8de94370638642f172265b0f1af27d816ea06d
Christiaan Meijer <c.meijer@esciencecenter.nl>
2015-07-28 16:23:27 +0200

    removed duplicate tests; all pass now

--- src/Dive/FrontBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Dive/FrontBundle/Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testIndex_returnsSomethingAtLeastContainingProjectTitle()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("DIVE")')->count() > 0);
     }
 }
